8ef030a74abdbcf515653d436bbbbc6af888382a
Harpal <harryharpalsingh@gmail.com>
2024-02-07 13:03:32 +0530

    added validations -- 7 Feb 2024

---
+++ validations/jquery-validations.js
@@ -0,0 +1,408 @@
+/*Script created by Harpal Singh -- instagram : @streetpro9 */
+let GlobalValidation = {
+    calculateDaysBetweenDates: function (date1, date2) {
+        /* Note : here expected format for date1 & date2 is : yyyy-MM-dd 
+           if you're passing date in format : dd-MM-yyyy then use Global.convertToYYYYMMDD()
+        */
+
+        // Convert the input strings to Date objects
+        const startDate = new Date(date1);
+        const endDate = new Date(date2);
+
+        // Calculate the difference in milliseconds
+        const timeDifference = endDate - startDate;
+
+        // Convert milliseconds to days (1 day = 24 * 60 * 60 * 1000 milliseconds)
+        const daysDifference = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
+
+        return daysDifference + 1;
+    },
+
+    calculateDaysBetweenDatesExcludingSundays: function (date1, date2) {
+        // Convert the input strings to Date objects
+        const startDate = new Date(date1);
+        const endDate = new Date(date2);
+
+        // Calculate the difference in milliseconds
+        const timeDifference = endDate - startDate;
+
+        // Convert milliseconds to days (1 day = 24 * 60 * 60 * 1000 milliseconds)
+        const totalDays = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
+
+        let excludedSundays = 0;
+
+        // Iterate through each day and check if it's a Sunday
+        for (let i = 0; i <= totalDays; i++) {
+            const currentDate = new Date(startDate);
+            currentDate.setDate(startDate.getDate() + i);
+
+            // Sunday is represented by 0 in JavaScript Date object
+            if (currentDate.getDay() === 0) {
+                excludedSundays++;
+            }
+        }
+
+        // Calculate the final result excluding Sundays
+        const daysDifferenceExcludingSundays = totalDays - excludedSundays;
+
+        return daysDifferenceExcludingSundays;
+
+        // Example usage:
+        //const startDateString = "2024-02-01";
+        //const endDateString = "2024-02-29";
+
+        //const daysBetween = calculateDaysBetweenDatesExcludingSundays(startDateString, endDateString);
+
+        //console.log(`Number of days between ${startDateString} and ${endDateString} excluding Sundays: ${daysBetween} days`);
+    },
+
+    convertToYYYYMMDD: function (dateString) {
+        //dateString : dd-MM-yyyy
+        const [day, month, year] = dateString.split('-');
+        return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+    },
+
+    GetCountryList: function () {
+        return [
+            { "name": "Afghanistan", "code": "AF" },
+            { "name": "Åland Islands", "code": "AX" },
+            { "name": "Albania", "code": "AL" },
+            { "name": "Algeria", "code": "DZ" },
+            { "name": "American Samoa", "code": "AS" },
+            { "name": "AndorrA", "code": "AD" },
+            { "name": "Angola", "code": "AO" },
+            { "name": "Anguilla", "code": "AI" },
+            { "name": "Antarctica", "code": "AQ" },
+            { "name": "Antigua and Barbuda", "code": "AG" },
+            { "name": "Argentina", "code": "AR" },
+            { "name": "Armenia", "code": "AM" },
+            { "name": "Aruba", "code": "AW" },
+            { "name": "Australia", "code": "AU" },
+            { "name": "Austria", "code": "AT" },
+            { "name": "Azerbaijan", "code": "AZ" },
+            { "name": "Bahamas", "code": "BS" },
+            { "name": "Bahrain", "code": "BH" },
+            { "name": "Bangladesh", "code": "BD" },
+            { "name": "Barbados", "code": "BB" },
+            { "name": "Belarus", "code": "BY" },
+            { "name": "Belgium", "code": "BE" },
+            { "name": "Belize", "code": "BZ" },
+            { "name": "Benin", "code": "BJ" },
+            { "name": "Bermuda", "code": "BM" },
+            { "name": "Bhutan", "code": "BT" },
+            { "name": "Bolivia", "code": "BO" },
+            { "name": "Bosnia and Herzegovina", "code": "BA" },
+            { "name": "Botswana", "code": "BW" },
+            { "name": "Bouvet Island", "code": "BV" },
+            { "name": "Brazil", "code": "BR" },
+            { "name": "British Indian Ocean Territory", "code": "IO" },
+            { "name": "Brunei Darussalam", "code": "BN" },
+            { "name": "Bulgaria", "code": "BG" },
+            { "name": "Burkina Faso", "code": "BF" },
+            { "name": "Burundi", "code": "BI" },
+            { "name": "Cambodia", "code": "KH" },
+            { "name": "Cameroon", "code": "CM" },
+            { "name": "Canada", "code": "CA" },
+            { "name": "Cape Verde", "code": "CV" },
+            { "name": "Cayman Islands", "code": "KY" },
+            { "name": "Central African Republic", "code": "CF" },
+            { "name": "Chad", "code": "TD" },
+            { "name": "Chile", "code": "CL" },
+            { "name": "China", "code": "CN" },
+            { "name": "Christmas Island", "code": "CX" },
+            { "name": "Cocos (Keeling) Islands", "code": "CC" },
+            { "name": "Colombia", "code": "CO" },
+            { "name": "Comoros", "code": "KM" },
+            { "name": "Congo", "code": "CG" },
+            { "name": "Congo, The Democratic Republic of the", "code": "CD" },
+            { "name": "Cook Islands", "code": "CK" },
+            { "name": "Costa Rica", "code": "CR" },
+            { "name": "Cote D\"Ivoire", "code": "CI" },
+            { "name": "Croatia", "code": "HR" },
+            { "name": "Cuba", "code": "CU" },
+            { "name": "Cyprus", "code": "CY" },
+            { "name": "Czech Republic", "code": "CZ" },
+            { "name": "Denmark", "code": "DK" },
+            { "name": "Djibouti", "code": "DJ" },
+            { "name": "Dominica", "code": "DM" },
+            { "name": "Dominican Republic", "code": "DO" },
+            { "name": "Ecuador", "code": "EC" },
+            { "name": "Egypt", "code": "EG" },
+            { "name": "El Salvador", "code": "SV" },
+            { "name": "Equatorial Guinea", "code": "GQ" },
+            { "name": "Eritrea", "code": "ER" },
+            { "name": "Estonia", "code": "EE" },
+            { "name": "Ethiopia", "code": "ET" },
+            { "name": "Falkland Islands (Malvinas)", "code": "FK" },
+            { "name": "Faroe Islands", "code": "FO" },
+            { "name": "Fiji", "code": "FJ" },
+            { "name": "Finland", "code": "FI" },
+            { "name": "France", "code": "FR" },
+            { "name": "French Guiana", "code": "GF" },
+            { "name": "French Polynesia", "code": "PF" },
+            { "name": "French Southern Territories", "code": "TF" },
+            { "name": "Gabon", "code": "GA" },
+            { "name": "Gambia", "code": "GM" },
+            { "name": "Georgia", "code": "GE" },
+            { "name": "Germany", "code": "DE" },
+            { "name": "Ghana", "code": "GH" },
+            { "name": "Gibraltar", "code": "GI" },
+            { "name": "Greece", "code": "GR" },
+            { "name": "Greenland", "code": "GL" },
+            { "name": "Grenada", "code": "GD" },
+            { "name": "Guadeloupe", "code": "GP" },
+            { "name": "Guam", "code": "GU" },
+            { "name": "Guatemala", "code": "GT" },
+            { "name": "Guernsey", "code": "GG" },
+            { "name": "Guinea", "code": "GN" },
+            { "name": "Guinea-Bissau", "code": "GW" },
+            { "name": "Guyana", "code": "GY" },
+            { "name": "Haiti", "code": "HT" },
+            { "name": "Heard Island and Mcdonald Islands", "code": "HM" },
+            { "name": "Holy See (Vatican City State)", "code": "VA" },
+            { "name": "Honduras", "code": "HN" },
+            { "name": "Hong Kong", "code": "HK" },
+            { "name": "Hungary", "code": "HU" },
+            { "name": "Iceland", "code": "IS" },
+            { "name": "India", "code": "IN" },
+            { "name": "Indonesia", "code": "ID" },
+            { "name": "Iran, Islamic Republic Of", "code": "IR" },
+            { "name": "Iraq", "code": "IQ" },
+            { "name": "Ireland", "code": "IE" },
+            { "name": "Isle of Man", "code": "IM" },
+            { "name": "Israel", "code": "IL" },
+            { "name": "Italy", "code": "IT" },
+            { "name": "Jamaica", "code": "JM" },
+            { "name": "Japan", "code": "JP" },
+            { "name": "Jersey", "code": "JE" },
+            { "name": "Jordan", "code": "JO" },
+            { "name": "Kazakhstan", "code": "KZ" },
+            { "name": "Kenya", "code": "KE" },
+            { "name": "Kiribati", "code": "KI" },
+            { "name": "Korea, Democratic People\"S Republic of", "code": "KP" },
+            { "name": "Korea, Republic of", "code": "KR" },
+            { "name": "Kuwait", "code": "KW" },
+            { "name": "Kyrgyzstan", "code": "KG" },
+            { "name": "Lao People\"S Democratic Republic", "code": "LA" },
+            { "name": "Latvia", "code": "LV" },
+            { "name": "Lebanon", "code": "LB" },
+            { "name": "Lesotho", "code": "LS" },
+            { "name": "Liberia", "code": "LR" },
+            { "name": "Libyan Arab Jamahiriya", "code": "LY" },
+            { "name": "Liechtenstein", "code": "LI" },
+            { "name": "Lithuania", "code": "LT" },
+            { "name": "Luxembourg", "code": "LU" },
+            { "name": "Macao", "code": "MO" },
+            { "name": "Macedonia, The Former Yugoslav Republic of", "code": "MK" },
+            { "name": "Madagascar", "code": "MG" },
+            { "name": "Malawi", "code": "MW" },
+            { "name": "Malaysia", "code": "MY" },
+            { "name": "Maldives", "code": "MV" },
+            { "name": "Mali", "code": "ML" },
+            { "name": "Malta", "code": "MT" },
+            { "name": "Marshall Islands", "code": "MH" },
+            { "name": "Martinique", "code": "MQ" },
+            { "name": "Mauritania", "code": "MR" },
+            { "name": "Mauritius", "code": "MU" },
+            { "name": "Mayotte", "code": "YT" },
+            { "name": "Mexico", "code": "MX" },
+            { "name": "Micronesia, Federated States of", "code": "FM" },
+            { "name": "Moldova, Republic of", "code": "MD" },
+            { "name": "Monaco", "code": "MC" },
+            { "name": "Mongolia", "code": "MN" },
+            { "name": "Montserrat", "code": "MS" },
+            { "name": "Morocco", "code": "MA" },
+            { "name": "Mozambique", "code": "MZ" },
+            { "name": "Myanmar", "code": "MM" },
+            { "name": "Namibia", "code": "NA" },
+            { "name": "Nauru", "code": "NR" },
+            { "name": "Nepal", "code": "NP" },
+            { "name": "Netherlands", "code": "NL" },
+            { "name": "Netherlands Antilles", "code": "AN" },
+            { "name": "New Caledonia", "code": "NC" },
+            { "name": "New Zealand", "code": "NZ" },
+            { "name": "Nicaragua", "code": "NI" },
+            { "name": "Niger", "code": "NE" },
+            { "name": "Nigeria", "code": "NG" },
+            { "name": "Niue", "code": "NU" },
+            { "name": "Norfolk Island", "code": "NF" },
+            { "name": "Northern Mariana Islands", "code": "MP" },
+            { "name": "Norway", "code": "NO" },
+            { "name": "Oman", "code": "OM" },
+            { "name": "Pakistan", "code": "PK" },
+            { "name": "Palau", "code": "PW" },
+            { "name": "Palestinian Territory, Occupied", "code": "PS" },
+            { "name": "Panama", "code": "PA" },
+            { "name": "Papua New Guinea", "code": "PG" },
+            { "name": "Paraguay", "code": "PY" },
+            { "name": "Peru", "code": "PE" },
+            { "name": "Philippines", "code": "PH" },
+            { "name": "Pitcairn", "code": "PN" },
+            { "name": "Poland", "code": "PL" },
+            { "name": "Portugal", "code": "PT" },
+            { "name": "Puerto Rico", "code": "PR" },
+            { "name": "Qatar", "code": "QA" },
+            { "name": "Reunion", "code": "RE" },
+            { "name": "Romania", "code": "RO" },
+            { "name": "Russian Federation", "code": "RU" },
+            { "name": "RWANDA", "code": "RW" },
+            { "name": "Saint Helena", "code": "SH" },
+            { "name": "Saint Kitts and Nevis", "code": "KN" },
+            { "name": "Saint Lucia", "code": "LC" },
+            { "name": "Saint Pierre and Miquelon", "code": "PM" },
+            { "name": "Saint Vincent and the Grenadines", "code": "VC" },
+            { "name": "Samoa", "code": "WS" },
+            { "name": "San Marino", "code": "SM" },
+            { "name": "Sao Tome and Principe", "code": "ST" },
+            { "name": "Saudi Arabia", "code": "SA" },
+            { "name": "Senegal", "code": "SN" },
+            { "name": "Serbia and Montenegro", "code": "CS" },
+            { "name": "Seychelles", "code": "SC" },
+            { "name": "Sierra Leone", "code": "SL" },
+            { "name": "Singapore", "code": "SG" },
+            { "name": "Slovakia", "code": "SK" },
+            { "name": "Slovenia", "code": "SI" },
+            { "name": "Solomon Islands", "code": "SB" },
+            { "name": "Somalia", "code": "SO" },
+            { "name": "South Africa", "code": "ZA" },
+            { "name": "South Georgia and the South Sandwich Islands", "code": "GS" },
+            { "name": "Spain", "code": "ES" },
+            { "name": "Sri Lanka", "code": "LK" },
+            { "name": "Sudan", "code": "SD" },
+            { "name": "Suriname", "code": "SR" },
+            { "name": "Svalbard and Jan Mayen", "code": "SJ" },
+            { "name": "Swaziland", "code": "SZ" },
+            { "name": "Sweden", "code": "SE" },
+            { "name": "Switzerland", "code": "CH" },
+            { "name": "Syrian Arab Republic", "code": "SY" },
+            { "name": "Taiwan", "code": "TW" },
+            { "name": "Tajikistan", "code": "TJ" },
+            { "name": "Tanzania, United Republic of", "code": "TZ" },
+            { "name": "Thailand", "code": "TH" },
+            { "name": "Timor-Leste", "code": "TL" },
+            { "name": "Togo", "code": "TG" },
+            { "name": "Tokelau", "code": "TK" },
+            { "name": "Tonga", "code": "TO" },
+            { "name": "Trinidad and Tobago", "code": "TT" },
+            { "name": "Tunisia", "code": "TN" },
+            { "name": "Turkey", "code": "TR" },
+            { "name": "Turkmenistan", "code": "TM" },
+            { "name": "Turks and Caicos Islands", "code": "TC" },
+            { "name": "Tuvalu", "code": "TV" },
+            { "name": "Uganda", "code": "UG" },
+            { "name": "Ukraine", "code": "UA" },
+            { "name": "United Arab Emirates", "code": "AE" },
+            { "name": "United Kingdom", "code": "GB" },
+            { "name": "United States", "code": "US" },
+            { "name": "United States Minor Outlying Islands", "code": "UM" },
+            { "name": "Uruguay", "code": "UY" },
+            { "name": "Uzbekistan", "code": "UZ" },
+            { "name": "Vanuatu", "code": "VU" },
+            { "name": "Venezuela", "code": "VE" },
+            { "name": "Viet Nam", "code": "VN" },
+            { "name": "Virgin Islands, British", "code": "VG" },
+            { "name": "Virgin Islands, U.S.", "code": "VI" },
+            { "name": "Wallis and Futuna", "code": "WF" },
+            { "name": "Western Sahara", "code": "EH" },
+            { "name": "Yemen", "code": "YE" },
+            { "name": "Zambia", "code": "ZM" },
+            { "name": "Zimbabwe", "code": "ZW" }
+        ]
+    },
+
+    isInputEmpty: function (obj, message_) {
+        let inputValue = $(obj).val(); //here obj is textbox 
+        $(obj).siblings().not('input').remove();
+        if (inputValue.trim() === '' || parseFloat(inputValue) === 0) {
+            // Display error message
+            $(obj).addClass('error');
+            $(obj).after(`<br/><span style='padding-top:3px; display:flex; font-size:10pt; color:#D00;'>${message_}</span>`);
+            return false;
+        }
+        else {
+            // Clear the error message
+            $(obj).removeClass('error');
+            return true;
+        }
+    },
+
+    isValidEmailFormat: function (obj) {
+        let email = $(obj).val();
+        $(obj).siblings().not('input').remove();
+        // Regular expression for validating an Email
+        let emailRegex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;///^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (emailRegex.test(email)) {
+            $(obj).removeClass('error');
+            return true;
+        }
+        else {
+            $(obj).addClass('error');
+            $(obj).after(`<br/><span style='padding-top:3px; display:flex; font-size:10pt; color:#D00;'>Email address is invalid</span>`);
+            return false;
+        }
+    },
+
+    isValidDropdown: function (obj, message_) {
+        let inputValue = $(obj).val();
+        $(obj).siblings().not('input').remove();
+        if (inputValue.trim() == 0) {
+            // Display error message
+            $(obj).addClass('error');
+            $(obj).after(`<span style='padding-top:5px; display:flex; font-size:10pt; color:#D00;'>${message_}</span>`);
+            return false;
+        }
+        else {
+            // Clear the error message
+            $(obj).removeClass('error');
+            return true;
+        }
+    },
+
+    isValidCodePostalFormat: function (obj) {
+        let postalCode = $(obj).val();
+        $(obj).siblings().not('input').remove();
+        // Regular expression for validating an Postal Code
+        let postalCodePattern = /^\d{5}$/;
+
+        if (postalCodePattern.test(postalCode)) {
+            $(obj).removeClass('error');
+            return true;
+        }
+        else {
+            $(obj).addClass('error');
+            $(obj).after(`<br/><span style='padding-top:3px; display:flex; font-size:10pt; color:#D00;'>Veuillez saisir une code postal valide, e.g. 95190</span>`);
+            return false;
+        }
+    },
+
+};
+
+/*Example usage of validations*/
+
+function ValidateForm() {
+    let isValid = true;
+
+    // Validate input fields
+    if (!Global.isInputEmpty($("#textBoxID"), `Your custom error message here`)) {
+        isValid = false;
+    }
+
+    // Validate dropdowns
+    if (!Global.isValidDropdown($("#ddlSelect"), 'Your custom error message here')) {
+        isValid = false;
+    }
+
+    // Validate email 
+    if (!Global.isValidEmailFormat($("#txtEmail"))) {
+        isValid = false;
+    }
+
+    // Check if all validations passed before further processing
+    if (isValid) {
+        //execute your code
+    }
+    else {
+        // Validation failed
+    }
+}
